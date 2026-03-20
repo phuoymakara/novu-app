7cc284836cd6363b831627a220af29a8cbdaf771
Phuoy Makara <115135485+phuoymakara@users.noreply.github.com>
2026-03-20 16:46:44 +0700

    Delete apps-nuxtui directory

--- apps-nuxtui/app/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <UApp>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-    <GlobalConfirmModal />
-  </UApp>
-</template>
--- apps-nuxtui/auth.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '#auth-utils' {
-  interface User {
-    id: number
-    email: string
-    name: string
-  }
-}
-
-export {}
